a83382cf41bb2eecbcd342c6aa486ce5124eb276
Жерновой Сергей <szhernovoy@persp-santrek.local>
2016-06-13 14:34:42 +0300

    add yslovie

---
+++ PartOne/src/main/java/chapter1/condition/Point.java
@@ -0,0 +1,26 @@
+/**
+*@author Sergey Zhernovoy
+*@version 1.0.0
+*@since 13-06-2016
+*/
+package chapter1.condition;
+
+class Point{
+
+   private double x;
+   private double y;
+
+   public Point(double x, double y) {
+      this.x = x;
+      this.y = y;
+   }
+   
+   public double distanceTo(Point point) {
+      
+	return Math.sqrt((point.x - x) * (point.x - x) + (point.y - y) * (point.y - y));
+
+   }
+
+
+
+}
---
+++ PartOne/src/main/java/chapter1/condition/Triangle.java
@@ -0,0 +1,24 @@
+/**
+*@author Sergey Zhernovoy
+*@version 1.0.0
+*@since 13-06-2016
+*/
+package chapter1.condition;
+
+public class Triangle {
+   
+   public Point a;
+   public Point b;
+   public Point c;
+
+   public Triangle(Point a, Point b, Point c) {
+      this.a = a;
+      this.b = b;
+      this.c = c;
+   }
+
+   public double area() {
+      //calculate the triangle area
+      return -1;
+   }
+}
