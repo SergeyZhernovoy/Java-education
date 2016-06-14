462c8ac36cdb00184468d480a2f2a3337cf64f30
Жерновой Сергей <szhernovoy@persp-santrek.local>
2016-06-14 17:22:44 +0300

    add loop

---
+++ PartOne/src/main/java/chapter1/loop/Square.java
@@ -0,0 +1,30 @@
+/**
+*вычисляет значение квадратной функции
+*@author Sergey Zhernovoy
+*@since 14-06-2016
+*/
+package chapter1.loop
+
+public class Square {
+
+ private double a;
+ private double b;
+ private double c;
+ private double x1 = 0;
+ private double x2 = 10;
+ private double step = 1;
+
+ public Square(double [] args){
+ 
+	if(args.length )
+
+
+
+	
+
+	}
+
+public double calc(){
+} 
+
+}
