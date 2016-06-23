3ddb5d8fff05ca093aca58c6b12f707e974ce8e1
SergeyZhernovoy <s_jernov@mail.ru>
2016-06-24 00:28:28 +0300

    final task

---
+++ Part I/src/main/java/chapter1/finaltask/SubString.java
@@ -0,0 +1,30 @@
+/**
+*
+*@since 24-06-2016
+*@author Sergey Zhernovoy
+*
+*/
+
+package chapter1.finaltest;
+
+public class SubString {
+
+
+public boolean contains(String original, String subStr){
+
+
+
+}
+
+
+public static void main(Sting[] args){
+
+
+
+
+
+}
+
+
+
+}
