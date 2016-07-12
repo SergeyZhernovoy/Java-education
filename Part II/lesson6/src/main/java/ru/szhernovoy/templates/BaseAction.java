8566a531f466fabac2faa9ad96c02ca227fe5457
SergeyZhernovoy <s_jernov@mail.ru>
2016-07-12 23:20:48 +0300

    update lesson 6

--- Part II/lesson6/src/main/java/ru/szhernovoy/templates/BaseAction.java
+++ Part II/lesson6/src/main/java/ru/szhernovoy/templates/BaseAction.java
@@ -16,13 +16,9 @@ public abstract class BaseAction implements UserAction{
 		this.name = name;
 	}
 
-	public abstract int key();
-	
-	public abstract void execute(Input input, Tracker tracker);
-	
 	public String info(){
 		return String.format("%s.) %s", this.key(),this.name);
-	};
+	}
 	
 
 }
