6ab827befddd4e8805ba4a4afe2e5ee636b658a6
SergeyZhernovoy <s_jernov@mail.ru>
2016-07-17 23:16:01 +0300

    update lesson1

---
+++ PartIII/Lesson1/src/main/java/ru/szhernovoy/MinFromDoubleNumeric.java
@@ -0,0 +1,34 @@
+package ru.szhernovoy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by admin on 17.07.2016.
+ */
+public class MinFromDoubleNumeric {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count =0;
+        double[] num = new double[3];
+
+        do{
+            System.out.print("\nPlease , enter numeric: ");
+            try{
+                num[count++] = Double.parseDouble(br.readLine());
+            }
+            catch(NumberFormatException iexc){
+                System.out.println("Wrong numeric");
+            }
+        }while(count < 3);
+        double min = num[0];
+        for(int index =0;index < 3;index++){
+            if(num[index] < min){
+                min = num[index];
+            }
+        }
+        System.out.println("Minimum is "+min);
+    }
+}
