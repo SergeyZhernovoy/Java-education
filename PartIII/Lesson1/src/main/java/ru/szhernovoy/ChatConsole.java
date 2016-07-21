55fdd1a13a61fcd44f8746bb9e9b77b138b0e6be
SergeyZhernovoy <s_jernov@mail.ru>
2016-07-22 00:52:19 +0300

    update lesson1 chat console

--- PartIII/Lesson1/src/main/java/ru/szhernovoy/ChatConsole.java
+++ PartIII/Lesson1/src/main/java/ru/szhernovoy/ChatConsole.java
@@ -1,75 +1,123 @@
 package ru.szhernovoy;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.Reader;
+import java.io.*;
+import java.util.Random;
 
 /**
  * Created by admin on 19.07.2016.
  */
 public class ChatConsole {
     private final String STOP = "стоп";
-    private final String CONTINUE_CHAT = "продолжить";
+    private final String CONTINUE = "продолжить";
     private final String EXIT = "закончить";
-    private final String filename = "resources\\Chat.txt";
-    private FileInputStream fs;
+    private final String FILEPHRASE = "D:\\temp\\Chat.txt";
+    private BufferedReader messageReader;
+    private StringBuilder chatMessages;
+    private String[] phraseArray;
 
-    public ChatConsole(){
-
+    public ChatConsole(Reader in){
+        this.messageReader = new BufferedReader(in);
+        this.chatMessages = new StringBuilder();
     }
 
-    public void init(Reader in){
-        BufferedReader br = new BufferedReader(in);
-        do{
-
+    public void init()  {
+        BufferedReader fileReader;
+        try {
+            File file =new File(FILEPHRASE);
+            if(!file.exists()){
+                file.createNewFile();
+                String data = " This content will append to the end of the file";
+                BufferedWriter bufferWritter = new BufferedWriter(new FileWriter(file.getName(),true));
+                bufferWritter.write(data);
+                bufferWritter.close();
+            }
+            fileReader  = new BufferedReader(new FileReader(FILEPHRASE));
+            String sCurrentLine;
+            int position = 0;
+            this.phraseArray = new String[5];
+            while ((sCurrentLine = fileReader.readLine()) != null) {
+                 if(position == phraseArray.length){
+                     this.phraseArray = this.getDoubleArray(phraseArray);
+                 }
+                 this.phraseArray[position++] = sCurrentLine;
+            }
+            String[] temp = new String[position];
+            System.arraycopy(this.phraseArray,0,temp,0,position);
+            this.phraseArray = temp;
+            fileReader.close();
         }
-        while(true);
+       catch (FileNotFoundException e) {
+            System.out.println(String.format("File %s not found or not exist",FILEPHRASE));
+       }
+        catch (IOException e) {
+            e.printStackTrace();
+       }
     }
 
-    public void writeMessage(String message){
+    private String[] getDoubleArray(String[] array){
+        int doubleSize = array.length*2;
+        String[] doubleArray = new String[doubleSize];
+        System.arraycopy(array,0,doubleArray,0,array.length);
+        return doubleArray;
+    }
 
+    public String getRandomLine(){
+        String sCurrentLine;
+        //int random = new Random().nextInt(phraseArray.length);
+        return phraseArray[new Random().nextInt(phraseArray.length)];
+    }
+
+    private String getInput(){
+
+        String recieveMessage = "Error reading";
+        try{
+            recieveMessage = messageReader.readLine();
+
+        } catch (IOException e) {
+            System.out.println("Error input reading");
+            e.printStackTrace();
+        }
+        return  recieveMessage;
+    }
+
+    public void work(){
+        String input,phrase;
+        boolean getPrhase = true;
+        System.out.println("Welcome to chat...");
+        System.out.print("you :");
+        while(!(input = this.getInput()).equalsIgnoreCase(EXIT)){
+            this.setString(input);
+
+            if(input.equalsIgnoreCase(STOP)){
+               getPrhase = false;
+            }
+
+            if(input.equalsIgnoreCase(CONTINUE)){
+                getPrhase = true;
+            }
+
+            if(getPrhase){
+                phrase =  this.getRandomLine();
+                this.setString(phrase);
+                System.out.println(String.format("answer: %s",phrase));
+            }
+            System.out.print("you :");
+        }
+        this.setString(input);
+
+        LogWriter lgwr = new LogWriter();
+        lgwr.init();
+        lgwr.writeLog(chatMessages.toString());
+    }
+
+    public void setString(String msg){
+        this.chatMessages.append(String.format("\n%s",msg));
     }
 
     public static void main(String[] args) {
-        ChatConsole cs = new ChatConsole();
-
+        ChatConsole cht = new ChatConsole(new InputStreamReader(System.in));
+        cht.init();
+        cht.work();
     }
-
-    try (BufferedReader br = new BufferedReader(new FileReader("C:\\testing.txt")))
-    {
-
-        String sCurrentLine;
-
-        while ((sCurrentLine = br.readLine()) != null) {
-            System.out.println(sCurrentLine);
-        }
-
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-
-    File file = new File("c:/newfile.txt");
-    String content = "This is the text content";
-
-		try (FileOutputStream fop = new FileOutputStream(file)) {
-
-        // if file doesn't exists, then create it
-        if (!file.exists()) {
-            file.createNewFile();
-        }
-
-        // get the content in bytes
-        byte[] contentInBytes = content.getBytes();
-
-        fop.write(contentInBytes);
-        fop.flush();
-        fop.close();
-
-        System.out.println("Done");
-
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-}
 
 }
---
+++ PartIII/Lesson1/src/main/java/ru/szhernovoy/LogWriter.java
@@ -0,0 +1,40 @@
+package ru.szhernovoy;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Created by admin on 21.07.2016.
+ */
+public class LogWriter {
+    private final String FILENAME = "D:\\temp\\log.txt";
+    private BufferedWriter bw;
+
+    public void init(){
+        File file = new File(FILENAME);
+        try {
+            if (!file.exists()) {
+
+                file.createNewFile();
+            }
+            this.bw = new BufferedWriter(new FileWriter(file.getAbsoluteFile()));
+        } catch (IOException e) {
+               System.out.println("Error crate log.txt ");
+               e.printStackTrace();
+        }
+    }
+
+    public void writeLog(String message){
+
+        try {
+            bw.write(message);
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
