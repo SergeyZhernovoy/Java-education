54d5123a8f21d6015860d9d4dfbddeb848ead514
szhernovoy <szhernov@gmail.com>
2016-08-01 14:33:08 +0300

    update SRP

--- PartIV/lesson1/src/main/java/ru/szhernovoy/tracker/models/Item.java
+++ PartIV/lesson1/src/main/java/ru/szhernovoy/tracker/models/Item.java
@@ -12,43 +12,82 @@ class Item {
 	private String name;
 	private String description;
 	private long create;
-	
+
+	/**
+	 * Setting initial parameters while create
+	 * @param name
+	 * @param descr
+	 * @param create
+     */
 	public Item(String name, String descr, long create){
 		this.name = name;
 		this.description = descr;
 		this.create = create;
 	}
-	
+
+	/**
+	 * Setting field name
+	 * @param name
+     */
 	public void setName(String name){
 			this.name = name;
 	}
-	
+
+	/**
+	 * Setting field description.
+	 * @param descr
+     */
 	public void setDescr(String descr){
 			this.description = descr;
 	}
-	
+
+	/**
+	 * Setting field data time create.
+	 * @param data
+     */
 	public void setData(long data){
 			this.create = data;
 	}
-	
+
+	/**
+	 * Setting id.
+	 * @param id
+     */
 	public void setId(String id){
 			this.id = id;
 	}
-	
+
+	/**
+	 * Get field name.
+	 * @return name
+     */
 	public String getName(){
 		return this.name;	
 	}
-	
+
+	/**
+	 * Get field description.
+	 * @return decription
+     */
 	public String getDescr(){
 		return this.description;	
 	}
-	
+
+	/**
+	 * Get field create.
+	 * @return create
+     */
 	public long getCreate(){
 		return this.create;	
 	}
-	
+
+	/**
+	 * Get field id.
+	 * @return id
+	 *
+     */
 	public String getId(){
-		return this.id;	
+		return this.id;
 	}
-	
+
 }
