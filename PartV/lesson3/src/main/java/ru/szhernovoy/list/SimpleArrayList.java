67f687f65284da95a42d392b4892b56dec71dc3b
DortMynder <szhernov@gmail.com>
2016-09-08 19:48:34 +0300

    add lesson3

---
+++ PartV/lesson3/src/main/java/ru/szhernovoy/list/SimpleArrayList.java
@@ -0,0 +1,13 @@
+package ru.szhernovoy.list;
+
+import java.util.Iterator;
+
+/**
+ * Created by Sergey on 08.09.2016.
+ */
+public class SimpleArrayList<T> implements SimpleContainer, Iterable<T> {
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+}
---
+++ PartV/lesson3/src/main/java/ru/szhernovoy/list/SimpleContainer.java
@@ -0,0 +1,7 @@
+package ru.szhernovoy.list;
+
+/**
+ * Created by Sergey on 08.09.2016.
+ */
+public interface SimpleContainer {
+}
