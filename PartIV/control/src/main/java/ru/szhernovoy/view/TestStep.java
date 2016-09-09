e1dfb6dfac65d2c547d62914dce0b7c59af92675
DortMynder <szhernov@gmail.com>
2016-09-09 13:09:12 +0300

    update  TicTac lucky step

--- PartIV/control/src/main/java/ru/szhernovoy/view/TestStep.java
+++ PartIV/control/src/main/java/ru/szhernovoy/view/TestStep.java
@@ -9,27 +9,103 @@ public class TestStep {
     private final int Y = 2;
     private final int Z = 0;
 
-    private int [] xy;
-
-    public TestStep(int[][] board) {
+    public TestStep(final int[][] board) {
         Board = board;
-        xy = new int[]{0, 0};
-
     }
 
     public int[] getLuckyStep(int symbol){
+        int [] xyLine = new int[]{-1, -1};
+        int [] xyColumne = new int[]{-1, -1};
+        int [] xyDiagonal = new int[]{-1, -1};
+        int [] xyDiagonalL = new int[]{-1, -1};
+        int [] xy = new int[]{-1, -1};
 
-        if(symbol == X || symbol == Y){
+
+        int accumaleteLine = 0;
+        int accumaleteColumne = 0;
+        int accumaleteDiagonal = 0;
+        int accumaleteDiagonalL = 0;
+
+        //up - down ; left - rigth
+        for(int i = 0, k =0, v = this.Board.length -1 ; i < this.Board.length;i++, k++,v--){
+
+            /**
+             * Calculate diagonal left to right
+             */
+
+            if(this.Board[i][k] == symbol){
+                accumaleteDiagonal++;
+            }
+            else{
+                xyDiagonal[0] = i;
+                xyDiagonal[1] = k;
+            }
+
+            /**
+             * Calculate diagonal right to left
+             */
+            if(this.Board[i][v] == symbol){
+                accumaleteDiagonalL++;
+            }
+            else{
+                xyDiagonalL[0] = i;
+                xyDiagonalL[1] = v;
+            }
 
 
+            for(int j = 0; j < this.Board.length;j++){
+                /**
+                 * Calculate line
+                  */
+                if(this.Board[i][j] == symbol){
+                    accumaleteLine++;
+                }
+                else{
+                    xyLine[0] = i;
+                    xyLine[1] = j;
+                }
 
+                /**
+                 * Calculate column
+                 */
+                if(this.Board[j][i] == symbol){
+                    accumaleteColumne++;
+                }
+                else{
+                    xyColumne[0] = j;
+                    xyColumne[1] = i;
+                }
+            }
 
+            if(accumaleteLine == 2  ){
+               xy = xyLine;
+               break;
+            }
+            else{
+                xyLine[0] = -1;
+                xyLine[1] = -1;
+                accumaleteLine = 0;
+            }
 
-
+            if(accumaleteColumne == 2  ){
+                xy = xyColumne;
+                break;
+            }
+            else{
+                xyColumne[0] = -1;
+                xyColumne[1] = -1;
+                accumaleteColumne = 0;
+            }
         }
-        else{
-            xy[0] = 0;
-            xy[1] = 0;
+
+        if(xy[0] == -1 && xy[1] ==-1 ){
+            if(accumaleteDiagonal == 2  ){
+                xy = xyDiagonal;
+            }
+
+            if(accumaleteDiagonalL == 2  ){
+                xy = xyDiagonalL;
+            }
         }
 
         return xy;
---
+++ PartIV/control/src/test/java/ru/szhernovoy/view/TestStepTest.java
@@ -0,0 +1,27 @@
+package ru.szhernovoy.view;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Sergey on 09.09.2016.
+ */
+public class TestStepTest {
+    @Test
+    public void getLuckyStep() throws Exception {
+
+        int[][] board = new int[][]{
+                {1,1,0},
+                {0,0,0},
+                {0,2,1}
+        };
+        int[] xy = new int[]{0,2};
+        TestStep test = new TestStep(board);
+        int[] xyTest = test.getLuckyStep(1);
+        Assert.assertThat(true,is((xyTest[0] == xy[0]) && (xyTest[1] == xy[1])));
+    }
+
+}
