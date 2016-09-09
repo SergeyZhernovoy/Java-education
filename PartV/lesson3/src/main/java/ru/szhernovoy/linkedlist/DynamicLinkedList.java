179b25aef39bf651371c780f240b07168b2b4970
DortMynder <szhernov@gmail.com>
2016-09-09 18:30:12 +0300

    update  Dynamic Linked List

---
+++ PartV/lesson3/src/main/java/ru/szhernovoy/linkedlist/DynamicLinkedList.java
@@ -0,0 +1,30 @@
+package ru.szhernovoy.linkedlist;
+
+import ru.szhernovoy.list.Container;
+
+import java.util.Iterator;
+
+/**
+ * Created by Sergey on 09.09.2016.
+ */
+public class DynamicLinkedList<T> implements Container<T> {
+    @Override
+    public void add(T t) {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    /**
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+}
