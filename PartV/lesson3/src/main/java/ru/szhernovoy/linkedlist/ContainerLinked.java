027e564f194801bd7273d016f91996c5685f1ab4
DortMynder <szhernov@gmail.com>
2016-09-11 21:21:43 +0300

    update  Dynamic Linked List

---
+++ PartV/lesson3/src/main/java/ru/szhernovoy/linkedlist/ContainerLinked.java
@@ -0,0 +1,9 @@
+package ru.szhernovoy.linkedlist;
+
+/**
+ * Created by Sergey on 08.09.2016.
+ */
+public interface ContainerLinked<E> extends Iterable<E> {
+    boolean add(E e);
+    E get(int index);
+}
--- PartV/lesson3/src/main/java/ru/szhernovoy/linkedlist/DynamicLinkedList.java
+++ PartV/lesson3/src/main/java/ru/szhernovoy/linkedlist/DynamicLinkedList.java
@@ -1,21 +1,45 @@
 package ru.szhernovoy.linkedlist;
-
-import ru.szhernovoy.list.Container;
-
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * Created by Sergey on 09.09.2016.
  */
-public class DynamicLinkedList<T> implements Container<T> {
-    @Override
-    public void add(T t) {
+public class DynamicLinkedList<T> implements ContainerLinked<T> {
 
+    private Node<T> first;
+    private Node<T> last;
+    private int size = 0;
+
+
+    @Override
+    public boolean add(T t) {
+        Node<T> next = this.last;
+        Node<T> newNode = new Node<>(t,next,null);
+        this.last = newNode;
+        if(next == null){
+          first = newNode;
+        }
+        else{
+          next.next = next;
+        }
+        this.size++;
+        return true;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        Node<T> element = first;
+        if(this.size != 0 && index <= this.size){
+           for(int i = 0; i < index; i++){
+               element = element.next;
+           }
+        }
+        else{
+            throw new NoSuchElementException("Not elements in linked list");
+        }
+
+        return (T) element.item;
     }
 
     /**
@@ -25,6 +49,67 @@ public class DynamicLinkedList<T> implements Container<T> {
      */
     @Override
     public Iterator<T> iterator() {
-        return null;
+        return new LinkedIterator<T>();
     }
+
+
+    private class Node<T>{
+        T item;
+        Node<T> next;
+        Node<T> prev;
+        public Node(T item, Node<T> next, Node<T> prev){
+            this.item = item;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    private class LinkedIterator<T> implements Iterator<T>{
+
+        /**
+         * Returns {@code true} if the iteration has more elements.
+         * (In other words, returns {@code true} if {@link #next} would
+         * return an element rather than throwing an exception.)
+         *
+         * @return {@code true} if the iteration has more elements
+         */
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        /**
+         * Returns the next element in the iteration.
+         *
+         * @return the next element in the iteration
+         * @throws NoSuchElementException if the iteration has no more elements
+         */
+        @Override
+        public T next() {
+            return null;
+        }
+
+        /**
+         * Removes from the underlying collection the last element returned
+         * by this iterator (optional operation).  This method can be called
+         * only once per call to {@link #next}.  The behavior of an iterator
+         * is unspecified if the underlying collection is modified while the
+         * iteration is in progress in any way other than by calling this
+         * method.
+         *
+         * @throws UnsupportedOperationException if the {@code remove}
+         *                                       operation is not supported by this iterator
+         * @throws IllegalStateException         if the {@code next} method has not
+         *                                       yet been called, or the {@code remove} method has already
+         *                                       been called after the last call to the {@code next}
+         *                                       method
+         * @implSpec The default implementation throws an instance of
+         * {@link UnsupportedOperationException} and performs no other action.
+         */
+        @Override
+        public void remove() {
+
+        }
+    }
+
 }
