cd3c4214c7e71c6f7612ef9944a8865b2ce03b5e
SergeyZhernovoy <s_jernov@mail.ru>
2016-09-17 00:18:30 +0300

    add Map (exc. №1)

---
+++ PartV/lesson5/src/main/java/ru/szhernovoy/map/User.java
@@ -0,0 +1,39 @@
+package ru.szhernovoy.map;
+
+import java.util.Calendar;
+
+/**
+ * Created by admin on 17.09.2016.
+ */
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (children != user.children) return false;
+        if (!name.equals(user.name)) return false;
+        return birthday.equals(user.birthday);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + children;
+        result = 31 * result + birthday.hashCode();
+        return result;
+    }
+}
