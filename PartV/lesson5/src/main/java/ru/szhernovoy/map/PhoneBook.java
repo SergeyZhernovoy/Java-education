4f0253d70fa459e57ca1996aa7ecb0359729976e
DortMynder <szhernov@gmail.com>
2016-09-21 21:26:59 +0300

    update  exc 8

--- PartV/lesson5/src/main/java/ru/szhernovoy/map/PhoneBook.java
+++ PartV/lesson5/src/main/java/ru/szhernovoy/map/PhoneBook.java
@@ -21,7 +21,7 @@ public class PhoneBook<T,V>  {
     public boolean insert(T key, V value){
 
         if(this.table.length == 0 || this.table == null || size > (this.table.length -1)){
-            this.resize();
+            this.table = this.resize();
         }
 
         return addValue(hash(key),key,value) == null;
@@ -30,7 +30,7 @@ public class PhoneBook<T,V>  {
     final V addValue(int hash, T key, V value) {
 
         Node<T, V> current;
-        int index = this.indexOfTable(hash);
+        int index = this.indexOfTable(hash,this.table.length);
         if ((current = this.table[index]) == null) {
             this.table[index] = newNode(hash, key, value, null);
         } else {
@@ -54,8 +54,8 @@ public class PhoneBook<T,V>  {
         return null;
     }
 
-    private int indexOfTable(int hash){
-        return  (this.table.length - 1)& hash;
+    private int indexOfTable(int hash, int length){
+        return  (length - 1)& hash;
     }
 
     // Create a regular (non-tree) node
@@ -67,144 +67,75 @@ public class PhoneBook<T,V>  {
 
         int newCapacity = this.table.length*2;
         Node<T,V>[] newTab = (Node<T,V>[])new Node[newCapacity];
-            for (int j = 0; j < this.table.length; ++j) {
-                Node<T,V> e;
-                if ((e = this.table[j]) != null) {
-                    Node<T,V> next;
-                    int hash = ;
-                    do {
-                        next  = e.next;
-                        hash  = hash(e.key)
-                        if ((e.hash & oldCap) == 0) {
-                            if (loTail == null)
-                                loHead = e;
-                            else
-                                loTail.next = e;
-                            loTail = e;
-                        }
-                        else {
-                            if (hiTail == null)
-                                hiHead = e;
-                            else
-                                hiTail.next = e;
-                            hiTail = e;
-                        }
-                    } while ((e = next) != null);
-
-
-
-
-
-
-
-
-
-
-                    oldTab[j] = null;
-                    if (e.next == null)
-                        newTab[e.hash & (newCap - 1)] = e;
-
-                    else { // preserve order
-                        Node<K,V> loHead = null, loTail = null;
-                        Node<K,V> hiHead = null, hiTail = null;
-                        Node<K,V> next;
-                        do {
-                            next = e.next;
-                            if ((e.hash & oldCap) == 0) {
-                                if (loTail == null)
-                                    loHead = e;
-                                else
-                                    loTail.next = e;
-                                loTail = e;
-                            }
-                            else {
-                                if (hiTail == null)
-                                    hiHead = e;
-                                else
-                                    hiTail.next = e;
-                                hiTail = e;
-                            }
-                        } while ((e = next) != null);
-                        if (loTail != null) {
-                            loTail.next = null;
-                            newTab[j] = loHead;
-                        }
-                        if (hiTail != null) {
-                            hiTail.next = null;
-                            newTab[j + oldCap] = hiHead;
-                        }
-                    }
-                }
+        for (int j = 0; j < this.table.length; ++j) {
+            Node<T, V> e;
+            if ((e = this.table[j]) != null) {
+                newTab[indexOfTable(e.hash,newCapacity)] = e;
             }
+        }
 
-        return table;
+        return newTab;
     }
 
-    public V get(Object key){
+    public V get(T key){
         Node<T,V> e;
         return (e = getNode(hash(key), key)) == null ? null : e.value;
     }
 
-    final Node<T,V> getNode(int hash, Object key) {
-//        Node<T,V>[] tab; Node<T,V> first, e; int n; T k;
-//        if ((tab = table) != null && (n = tab.length) > 0 &&
-//                (first = tab[(n - 1) & hash]) != null) {
-//            if (first.hash == hash && // always check first node
-//                    ((k = first.key) == key || (key != null && key.equals(k))))
-//                return first;
-//            if ((e = first.next) != null) {
-//                do {
-//                    if (e.hash == hash &&
-//                            ((k = e.key) == key || (key != null && key.equals(k))))
-//                        return e;
-//                } while ((e = e.next) != null);
-//            }
-//        }
+    final Node<T,V> getNode(int hash, T key) {
+
+        Node<T,V> first, e;
+        int n; T k;
+        if (this.table != null && this.table.length > 0 && (first = this.table[indexOfTable(hash,this.table.length)]) != null) {
+            if (first.hash == hash && ((k = first.key) == key || (key != null && key.equals(k))))
+                return first;
+            if ((e = first.next) != null) {
+                do {
+                    if (e.hash == hash &&  ((k = e.key) == key || (key != null && key.equals(k))))
+                        return e;
+                } while ((e = e.next) != null);
+            }
+        }
         return null;
     }
 
     public boolean delete(T key){
-        //removeNode(hash(key), key) != null;
-        return true;
+        return removeNode(hash(key), key) != null;
+
     }
 
-    final Node<T,V> removeNode(int hash, Object key) {
-/*        Node<K,V>[] tab; Node<K,V> p; int n, index;
-        if ((tab = table) != null && (n = tab.length) > 0 &&
-                (p = tab[index = (n - 1) & hash]) != null) {
-            Node<K,V> node = null, e; K k; V v;
-            if (p.hash == hash &&
-                    ((k = p.key) == key || (key != null && key.equals(k))))
+    final Node<T,V> removeNode(int hash, T key) {
+
+        Node<T,V> p = null,e;
+        Node<T,V> node = null;
+
+        if (this.table != null && this.table.length > 0 && (p = this.table[indexOfTable(hash,this.table.length)]) != null) {
+
+            if (p.hash == hash &&  (p.key == key || (key != null && key.equals(p.key)))) {
                 node = p;
-            else if ((e = p.next) != null) {
-                if (p instanceof TreeNode)
-                    node = ((TreeNode<K,V>)p).getTreeNode(hash, key);
-                else {
-                    do {
-                        if (e.hash == hash &&
-                                ((k = e.key) == key ||
-                                        (key != null && key.equals(k)))) {
-                            node = e;
-                            break;
-                        }
-                        p = e;
-                    } while ((e = e.next) != null);
+            }
+
+            else {
+                while ((e = p.next) != null){
+                    if (e.hash == hash && e.key == key ||  (key != null && key.equals(e.key))) {
+                        node = e;
+                        break;
+                    }
+                    p = e;
                 }
             }
-            if (node != null && (!matchValue || (v = node.value) == value ||
-                    (value != null && value.equals(v)))) {
-                if (node instanceof TreeNode)
-                    ((TreeNode<K,V>)node).removeTreeNode(this, tab, movable);
-                else if (node == p)
-                    tab[index] = node.next;
-                else
-                    p.next = node.next;
-                ++modCount;
-                --size;
-                afterNodeRemoval(node);
-                return node;
+        }
+        if (node != null ) {
+            if (node == p) {
+                this.table[indexOfTable(hash, this.table.length)] = node.next;
             }
-        }*/
+            else{
+                p.next = node.next;
+            }
+            --size;
+            return node;
+        }
+
         return null;
     }
 
@@ -240,7 +171,7 @@ public class PhoneBook<T,V>  {
          */
         @Override
         public T getKey() {
-            return null;
+            return this.key;
         }
 
         /**
@@ -255,7 +186,7 @@ public class PhoneBook<T,V>  {
          */
         @Override
         public V getValue() {
-            return null;
+            return this.value;
         }
 
         /**
@@ -280,57 +211,8 @@ public class PhoneBook<T,V>  {
          */
         @Override
         public V setValue(V value) {
-            return null;
-        }
-
-
-    }
-
-    private class IterNode<V> implements Iterator<V>{
-
-        /**
-         * Returns {@code true} if the iteration has more elements.
-         * (In other words, returns {@code true} if {@link #next} would
-         * return an element rather than throwing an exception.)
-         *
-         * @return {@code true} if the iteration has more elements
-         */
-        @Override
-        public boolean hasNext() {
-            return false;
-        }
-
-        /**
-         * Returns the next element in the iteration.
-         *
-         * @return the next element in the iteration
-         * @throws  if the iteration has no more elements
-         */
-        @Override
-        public V next() {
-            return null;
-        }
-
-        /**
-         * Removes from the underlying collection the last element returned
-         * by this iterator (optional operation).  This method can be called
-         * only once per call to {@link #next}.  The behavior of an iterator
-         * is unspecified if the underlying collection is modified while the
-         * iteration is in progress in any way other than by calling this
-         * method.
-         *
-         * @throws UnsupportedOperationException if the {@code remove}
-         *                                       operation is not supported by this iterator
-         * @throws IllegalStateException         if the {@code next} method has not
-         *                                       yet been called, or the {@code remove} method has already
-         *                                       been called after the last call to the {@code next}
-         *                                       method
-         * @implSpec The default implementation throws an instance of
-         * {@link UnsupportedOperationException} and performs no other action.
-         */
-        @Override
-        public void remove() {
-
+            this.value = value;
+            return this.value;
         }
     }
 
@@ -380,10 +262,6 @@ public class PhoneBook<T,V>  {
         public void remove() {
 
         }
-    }
-
-    public boolean contains(){
-        return true;
     }
 
 }
