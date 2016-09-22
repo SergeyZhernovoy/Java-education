1ccf98b7847149a106afa379c132565bd84d864f
szhernovoy <szhernov@gmail.com>
2016-09-22 11:28:08 +0300

    add lesson 6 (Tree)

---
+++ PartV/lesson6/src/main/java/ru/szhernovoy/tree/SimpleTree.java
@@ -0,0 +1,7 @@
+package ru.szhernovoy.tree;
+
+/**
+ * Created by szhernovoy on 22.09.2016.
+ */
+public class SimpleTree {
+}
---
+++ PartV/lesson6/src/test/java/ru/szhernovoy/tree/SimpleTreeTest.java
@@ -0,0 +1,10 @@
+package ru.szhernovoy.tree;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by szhernovoy on 22.09.2016.
+ */
+public class SimpleTreeTest {
+
+}
