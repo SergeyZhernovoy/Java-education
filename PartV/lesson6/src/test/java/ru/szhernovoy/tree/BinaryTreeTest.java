1f8f2517f135afb0286b6c9c94046bfffaf74ed8
SergeyZhernovoy <s_jernov@mail.ru>
2016-09-25 23:23:35 +0300

    update Tree (part . 5 lesson 6 exc. 5)

--- PartV/lesson6/src/test/java/ru/szhernovoy/tree/BinaryTreeTest.java
+++ PartV/lesson6/src/test/java/ru/szhernovoy/tree/BinaryTreeTest.java
@@ -1,7 +1,9 @@
 package ru.szhernovoy.tree;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 /**
@@ -12,7 +14,21 @@ public class BinaryTreeTest {
     public void whenAddInBinaryTreeWeGetValueByKey() throws Exception {
 
         BinaryTree<Integer,String> phoneBook = new BinaryTree<>();
+        Integer one = 1;
+        String name1 = "Sergey";
+        phoneBook.put(one,name1);
+        phoneBook.put(2,"Nick");
+        phoneBook.put(3,"Petr");
+        phoneBook.put(4,"");
+        phoneBook.put(14,"Rame");
+        phoneBook.put(43,"Julia");
+        phoneBook.put(5,"Dasha");
+        phoneBook.put(32,"Lera");
+        phoneBook.put(3,"Dima");
+        phoneBook.put(6,"Sasha");
 
+        String result = phoneBook.get(3);
+        Assert.assertThat("Dima",is(result));
 
 
     }
