4f9fbea2f9d24100d5be57251f12a1c1895cedfe
SergeyZhernovoy <s_jernov@mail.ru>
2016-10-04 00:30:52 +0300

    update garbage collection lesson1

--- PartVI/lesson1/src/main/java/ru/szhernovoy/User.java
+++ PartVI/lesson1/src/main/java/ru/szhernovoy/User.java
@@ -1,7 +1,47 @@
 package ru.szhernovoy;
 
+
 /**
  * Created by szhernovoy on 03.10.2016.
  */
 public class User {
+   private String name;
+   private Integer age;
+
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println(String.format("User %s is finalize",this));
+
+    }
+
+    @Override
+    public String toString() {
+        return "name='" + name + '\'' +
+                ", age=" + age;
+    }
+
+    public void info(){
+        int mb = 1024 * 1024;
+        Runtime runtime = Runtime.getRuntime();
+        System.out.println("##### Heap utilization statistics [MB] #####");
+        System.out.println("Used memory: "+ (runtime.totalMemory() - runtime.freeMemory())/mb + "Mb");
+        System.out.println("Free memory: "+ (runtime.freeMemory())/mb + "Mb");
+        System.out.println("Total memory: "+ (runtime.totalMemory() )/mb + "Mb");
+        System.out.println("Max memory: "+ (runtime.maxMemory() )/mb + "Mb");
+    }
+
+    public static void main(String[] args) {
+        for (int index = 0; index < 10000; index++){
+            System.out.println(new User(String.valueOf(index),index));
+        }
+    }
+
 }
