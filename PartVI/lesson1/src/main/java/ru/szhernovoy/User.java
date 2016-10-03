8d074b8e6509f6ad9daa4c0fc05d04a68c20f01d
szhernovoy <szhernov@gmail.com>
2016-10-03 16:48:33 +0300

    add Part VI lesson1

---
+++ PartVI/lesson1/src/main/java/ru/szhernovoy/User.java
@@ -0,0 +1,7 @@
+package ru.szhernovoy;
+
+/**
+ * Created by szhernovoy on 03.10.2016.
+ */
+public class User {
+}
---
+++ PartVI/lesson1/src/test/java/ru/szhernovoy/UserTest.java
@@ -0,0 +1,10 @@
+package ru.szhernovoy;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by szhernovoy on 03.10.2016.
+ */
+public class UserTest {
+
+}
