afca45767a1252fff93a674f2b6f66afaa04e70f
szhernovoy <szhernov@gmail.com>
2016-10-26 16:39:26 +0300

    update lesson 4 block queue

---
+++ PartVII/lesson4/src/main/java/ru/szhernovoy/template/ProducerCustomer.java
@@ -0,0 +1,99 @@
+package ru.szhernovoy.template;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by szhernovoy on 26.10.2016.
+ */
+public class ProducerCustomer {
+
+    private AtomicInteger num = new AtomicInteger(0);
+    private final BlockingQueue<AtomicInteger> queue = new ArrayBlockingQueue<>(5);
+
+    public int getNum() {
+        return num.get();
+    }
+
+    public void add(){
+        synchronized (this.queue){
+            System.out.println(String.format("work Producer %s - add num %d",Thread.currentThread().getId(),this.num.get()));
+            try {
+                this.queue.put(this.num);
+                this.num.getAndIncrement();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            this.queue.notify();
+        }
+
+    }
+
+    public void getData()  {
+         synchronized (this.queue){
+            while(this.queue.isEmpty()){
+                try {
+                    System.out.println(String.format("wait Customer %s",Thread.currentThread().getId()));
+                    this.queue.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+             try {
+                 System.out.println(String.format("wait Customer %s - get num %d",Thread.currentThread().getId(),this.queue.take().get()));
+             } catch (InterruptedException e) {
+                 e.printStackTrace();
+             }
+
+         }
+
+    }
+
+    public static void main(String[] args) {
+
+        final ProducerCustomer info = new ProducerCustomer();
+        Thread producer = new Thread(){
+            /**
+             * If this thread was constructed using a separate
+             * <code>Runnable</code> run object, then that
+             * <code>Runnable</code> object's <code>run</code> method is called;
+             * otherwise, this method does nothing and returns.
+             * <p>
+             * Subclasses of <code>Thread</code> should override this method.
+             *
+             * @see #start()
+             * @see #stop()
+             * @see #(ThreadGroup, Runnable, String)
+             */
+            @Override
+            public void run() {
+                info.add();
+            }
+        };
+
+        Thread customer = new Thread(){
+            /**
+             * If this thread was constructed using a separate
+             * <code>Runnable</code> run object, then that
+             * <code>Runnable</code> object's <code>run</code> method is called;
+             * otherwise, this method does nothing and returns.
+             * <p>
+             * Subclasses of <code>Thread</code> should override this method.
+             *
+             * @see #start()
+             * @see #stop()
+             * @see #(ThreadGroup, Runnable, String)
+             */
+            @Override
+            public void run() {
+                info.getData();
+            }
+        };
+        customer.start();
+        producer.start();
+
+
+    }
+}
