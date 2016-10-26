9203bdefd17ed90bb0c850b455fd30fe6f9466f2
SergeyZhernovoy <s_jernov@mail.ru>
2016-10-26 23:30:10 +0300

    update ThreadPool

--- PartVII/lesson4/src/main/java/ru/szhernovoy/pool/ThreadPool.java
+++ PartVII/lesson4/src/main/java/ru/szhernovoy/pool/ThreadPool.java
@@ -8,6 +8,65 @@ import java.util.concurrent.BlockingQueue;
  */
 public class ThreadPool {
 
-    private BlockingQueue<Object> pool = new ArrayBlockingQueue<>(10);
+    private final int KERNEL = Runtime.getRuntime().availableProcessors();
+    private final BlockingQueue<Runnable> pool = new ArrayBlockingQueue<>(this.KERNEL);
+
+    public ThreadPool(){
+        for (int index = 0; index < this.pool.size(); index++){
+            this.pool.add(new Work());
+        }
+    }
+
+    public void add(Work work){
+
+        synchronized (this.pool){
+            this.pool.add(work);
+            this.pool.notifyAll();
+            System.out.println(String.format("Work -%s is add",Thread.currentThread().getId()));
+        }
+    }
+
+    public void execute(){
+
+        synchronized (this.pool){
+            while(this.pool.isEmpty()){
+                try {
+                    this.pool.wait();
+                    System.out.println(String.format("wait - current thread %s",Thread.currentThread().getId()));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            Runnable work = null;
+            while((work = this.pool.poll()) != null){
+                 System.out.println(String.format("work  - current thread %s",Thread.currentThread().getId()));
+                 new Thread(work).start();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        final ThreadPool threadPool = new ThreadPool();
+
+        Thread poolAdd = new Thread(){
+            @Override
+            public void run() {
+              threadPool.add(new Work());
+                threadPool.add(new Work());
+            }
+        };
+        Thread executorPool = new Thread(){
+            @Override
+            public void run() {
+              threadPool.execute();
+            }
+        };
+
+        poolAdd.start();
+        executorPool.start();
+
+    }
 
 }
