3096b29886d76837fcbc4bce66f20dec8f975622
szhernovoy <szhernov@gmail.com>
2016-11-03 11:21:15 +0300

    update control

--- PartVII/control/src/main/java/ru/szhernovoy/bomberman/model/Monster.java
+++ PartVII/control/src/main/java/ru/szhernovoy/bomberman/model/Monster.java
@@ -5,6 +5,7 @@ package ru.szhernovoy.bomberman.model;
  */
 public class Monster extends AbstractCharacter implements Runnable{
 
+    private Object lock = new Object();
 
     public Monster(String name, int id,Cell[][] cells,int x, int y) {
         super(name, id, cells,x ,y);
@@ -26,16 +27,13 @@ public class Monster extends AbstractCharacter implements Runnable{
                }
 
                if (this.next.getCharacter() == null) {
-                   synchronized (this.next) {
-                       synchronized (this.cells[xPosition][yPosition]) {
+                   synchronized (this.lock) {
                            this.next.setCharacter(this);
                            this.cells[xPosition][yPosition].erase();
                            System.out.println(String.format("%s do move from %d%d to %d%d %s",this.name,this.xPosition,this.yPosition,this.nextX,this.nextY,step));
                            this.xPosition = nextX;
                            this.yPosition = nextY;
                            makeStep = true;
-
-                       }
                    }
                }
                else{
