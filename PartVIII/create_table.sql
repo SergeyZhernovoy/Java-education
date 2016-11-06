20106d513a8456a98758df7ef2044525a6f77dff
SergeyZhernovoy <s_jernov@mail.ru>
2016-11-07 00:48:21 +0300

    create database

---
+++ PartVIII/create_table.sql
@@ -0,0 +1,6 @@
+﻿create table users(
+  id serial primary key,
+  login character varying(2000),
+  password character varying(2000),
+  create_date timestamp
+);
---
+++ PartVIII/start_scripts.cmd
@@ -0,0 +1,3 @@
+psql --username=postgres -f createDB.sql
+psql --username=postgres -f createable.sql
+psql --username=postgres -f fillingDB.sql
