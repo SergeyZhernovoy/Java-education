b528d3de275d01f1968a65e9e321b1de9140e629
SergeyZhernovoy <s_jernov@mail.ru>
2016-11-09 00:17:07 +0300

    add lesson 3

---
+++ PartVIII/lesson3/create_table_car.sql
@@ -0,0 +1,31 @@
+﻿--create transmission
+create table transmission(
+   transmission_id serial primary key,
+   name varchar(150) NOT NULL,
+   type_transmission smallint NOT NULL
+);
+
+
+--create engine
+create table engine(
+   engine_id serial primary key,
+   name varchar(150) NOT NULL,
+   type_engine smallint NOT NULL
+);
+
+--create box
+create table gear_box(
+   gear_box_id serial primary key, 
+   name varchar(150) NOT NULL,
+   type_box smallint NOT NULL
+);
+
+--create carcreate table gear_box(
+create table car(
+   car_id serial primary key,
+   name varchar(150) NOT NULL,
+   box_id integer references  gear_box(gear_box_id),
+   engine_id integer references   engine(engine_id),
+   transm_id integer references  transmission(transmission_id)
+   
+);
