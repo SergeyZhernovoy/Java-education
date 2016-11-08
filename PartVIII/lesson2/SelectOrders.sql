a72039d59111021f73e7aa466781f61afdc2693f
szhernovoy <szhernov@gmail.com>
2016-11-08 11:04:05 +0300

    update leeson 2 sql

---
+++ PartVIII/lesson2/SelectOrders.sql
@@ -0,0 +1,14 @@
+﻿--выберем ордера на продажу в статусе новый
+
+SELECT  
+	listOrders.order_id, 
+	listOrders.date_order, 
+	listOrders.number_order, 
+	Type.name, 
+	Status.name 
+FROM orders as listOrders
+INNER JOIN type_order as Type 
+ON listOrders.type_id = Type.type_id
+INNER JOIN status_order as Status 
+ON listOrders.status_id = Status.status_id
+WHERE Status.status_id = 1 AND Type.name = 'sale';
