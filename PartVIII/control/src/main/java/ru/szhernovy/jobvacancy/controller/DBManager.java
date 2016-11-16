de3ef9c7be5af8007ff78ec7ed94ff086707e4c7
szhernovoy <szhernov@gmail.com>
2016-11-16 17:21:29 +0300

    update control SQL JSOUP JDBC

--- PartVIII/control/src/main/java/ru/szhernovy/jobvacancy/controller/DBManager.java
+++ PartVIII/control/src/main/java/ru/szhernovy/jobvacancy/controller/DBManager.java
@@ -6,22 +6,25 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import ru.szhernovy.jobvacancy.model.*;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Set;
 
 
 public class DBManager {
     private static Logger log = LoggerFactory.getLogger(DBManager.class);
-    private final Set<Vacancy> storage;
+   // private final Set<Vacancy> storage;
 
-    public DBManager(Set<Vacancy> storage) {
-        this.storage = storage;
+    public DBManager() {
+
     }
 
     public Vacancy add(Vacancy vacancy){
         return vacancy;
     }
 
-
-
+    public long getFirstLoad(){
+        return  0;
+    }
 
 }
--- PartVIII/control/src/main/java/ru/szhernovy/jobvacancy/controller/HTMLParser.java
+++ PartVIII/control/src/main/java/ru/szhernovy/jobvacancy/controller/HTMLParser.java
@@ -8,25 +8,34 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
-import ru.szhernovy.jobvacancy.model.Vacancy;
 
 import java.io.IOException;
-import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class HTMLParser {
-    private final String REGEX = "\\[0-9]";
-    //private final Set<Vacancy> storage;
-    private static Logger log = LoggerFactory.getLogger(HTMLParser.class);
 
-    private String url = "http://www.sql.ru/forum/job-offers";
+    private final DBManager storage;
+    private static Logger log = LoggerFactory.getLogger(HTMLParser.class);
+    private long startDate;
+    private boolean firstLoad;
+
+    private static final String REGEX = "(\\$\\{)(\\w+)(\\})";
+    private static Pattern pattern = Pattern.compile(REGEX);
+
+    private String url = "http://www.sql.ru/forum/job-offers/";
+
+    public HTMLParser(DBManager storage) {
+        this.storage = storage;
+    }
 
     //public HTMLParser(Set<Vacancy> storage) {
        // this.storage = storage;
 
     //}
 
-    public Document getHtmlpage(String url) {
+    public Document getHtmlPage(String url) {
 
         Document doc = null;
         try{
@@ -49,51 +58,119 @@ public class HTMLParser {
     }
 
     public Elements getRowTable(Elements elements){
-        Elements elementsRow = elements.select("tr");
+        Elements elementsRow = elements.select("tr:has(.postslisttopic)");
         return  elementsRow;
     }
 
+    public void init(){
+        this.startDate = storage.getFirstLoad();
+        this.firstLoad =  startDate == 0 ? true : false;
+    }
+
+    public boolean checkVacancy(String textVacancy){
+        Matcher m = Pattern.compile(REGEX).matcher(textVacancy);
+        return m.find();
+    }
+
+
+
+    public long mainLoop(){
+
+        boolean next  = true;
+        int numberPage = 1;
+        Document document = getHtmlPage(String.format("%s/%d",this.url,numberPage++));
+        Elements rows = getRowTable(getTableHtml(document));
+        Elements oldRows = rows;
+        while(next){
+            for(Element element : rows){
+                Elements cols = element.select("td:not(.icon_cell)");
+                Elements sp = cols.select("span.closedTopic");
+                Elements refs = cols.select("a[href]");
+                if(sp.isEmpty()){
+                   if(checkVacancy(cols.get(0).text())){
+
+                           String linkVacancy = cols.get(0).select("a").attr("href");
+                           String nameVacancy = cols.get(0).text();
+                           String author      = cols.get(1).text();
+                           String linkAuthor  = cols.get(1).select("a").attr("href");
+                           int ask   = Integer.valueOf(cols.get(2).text());
+                           int view = Integer.valueOf(cols.get(3).text());
+                           long timeUpdate = convertation(Long.valueOf(cols.get(4).text()));
+
+
+                        }
+
+
+
+                }
+
+
+
+                System.out.println(refs.attr("href"));
+
+
+
+                if(firstLoad){
+
+
+
+                }
+              document = getHtmlPage(String.format("%s/%d",this.url,numberPage++));
+              rows = getRowTable(getTableHtml(document));
+              if(rows.equals(oldRows)){
+                 next = false;
+              }
+              else{
+                  oldRows = rows;
+              }
+        }
+
+
 
     public static void main(String[] args) {
+        DBManager storage = new DBManager();
+        HTMLParser parser = new HTMLParser(storage);
+        Document document = parser.getHtmlPage("http://www.sql.ru/forum/job-offers/1");
 
-        HTMLParser parser = new HTMLParser();
-        Document document = parser.getHtmlpage("http://www.sql.ru/forum/job-offers/9");
-
-        Elements rows = parser.getRowTable(parser.getTableHtml(document));
-        for(Element element : rows){
-            Elements cols = element.select("td");
-            System.out.println("-------------------------------------------------------");
-
-            int currentCol = 0;
-            for(Element col : cols){
-                System.out.println(col);
-                if(currentCol == 5) {
-                    currentCol =0;
+     boolean firstLoad = true;
+        Elements rows =  parser.getRowTable(parser.getTableHtml(document));
+        for (Element row : rows){
+            Elements cols = row.select("td:not(.icon_cell)");
+            Elements sp = cols.select("span.closedTopic");
+            Elements refs = cols.select("a");
+            if(sp.isEmpty()){
+                for(int index =0; index < cols.size(); index++){
+                    switch (index){
+                        case 0:
+                            System.out.println(cols.get(index).text());
+                            System.out.println(refs.attr("href"));
+                            break;
+                        case 1:
+                            System.out.println(cols.get(index).text());
+                            System.out.println(cols.get(index).select("a").attr("href"));
+                            break;
+                        case 2:
+                            System.out.println(cols.get(index));
+                            break;
+                        case 3:
+                            System.out.println(cols.get(index));
+                            break;
+                        case 4:
+                            System.out.println(cols.get(index));
+                            break;
+                        default: throw new UnknownError();
+                    }
                 }
 
-
-                if(currentCol == 1) {
-                     col.attributes();
-                }
-                currentCol++;
+                System.out.println("----------------------");
             }
-
-
-        }
-        /*
-        for (int i = 0; i < rows.size(); i++) {
-            Element row = rows.get(i);
-            Elements cols = row.select("td");
-
-            for (Element e: cols){
-                System.out.println(e.text());
             }
-        }*/
-
-
 
 
     }
 
 
 }
+
+    private long convertation(Long aLong) {
+    }
