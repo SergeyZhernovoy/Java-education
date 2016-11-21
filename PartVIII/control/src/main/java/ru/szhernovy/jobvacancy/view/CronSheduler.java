7d3e75ee3b9dbfc3c643ed7e36a9e0d3a0695482
SergeyZhernovoy <s_jernov@mail.ru>
2016-11-22 00:16:40 +0300

    update control part 8 cron

---
+++ PartVIII/control/src/main/java/ru/szhernovy/jobvacancy/view/CronSheduler.java
@@ -0,0 +1,11 @@
+package ru.szhernovy.jobvacancy.view;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+/**
+ * Created by admin on 22.11.2016.
+ */
+public class CronSheduler {
+    private final static Logger log = LoggerFactory.getLogger(CronSheduler.class);
+}
--- PartVIII/control/src/main/java/ru/szhernovy/jobvacancy/view/JobSheduler.java
+++ PartVIII/control/src/main/java/ru/szhernovy/jobvacancy/view/JobSheduler.java
@@ -18,50 +18,5 @@ public class JobSheduler implements Job{
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
 
     }
-/*
-    public Executor(){
-    //   String path = this.getClass().getClassLoader().getResource("vacancy.properties").getPath();
-    }
-
-    public static void main(String[] args) {
-
-        SchedulerFactory schedFact = new org.quartz.impl.StdSchedulerFactory();
-
-        Scheduler sched = null;
-        try {
-            sched = schedFact.getScheduler();
-        } catch (SchedulerException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            sched.start();
-        } catch (SchedulerException e) {
-            e.printStackTrace();
-        }
-
-        // define the job and tie it to our HelloJob class
-        JobDetail job = newJob(HelloJob.class)
-                .withIdentity("myJob", "group1")
-                .build();
-
-        // Trigger the job to run now, and then every 40 seconds
-        Trigger trigger = newTrigger()
-                .withIdentity("myTrigger", "group1")
-                .startNow()
-                .withSchedule(simpleSchedule()
-                        .withIntervalInSeconds(40)
-                        .repeatForever())
-                .build();
-
-        // Tell quartz to schedule the job using our trigger
-        try {
-            sched.scheduleJob(job, trigger);
-        } catch (SchedulerException e) {
-            e.printStackTrace();
-        }
-    }
-
-*/
 
 }
