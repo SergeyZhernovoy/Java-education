dd7a210db5e0f9eee6fd3f957a0cdf7e31097036
SergeyZhernovoy <s_jernov@mail.ru>
2016-12-14 00:35:29 +0300

    add lesson 7 - Mockito

---
+++ PartVIIII/lesson6/src/test/java/ru/szhernovoy/controllers/AuthFilterTest.java
@@ -0,0 +1,10 @@
+package ru.szhernovoy.controllers;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by admin on 14.12.2016.
+ */
+public class AuthFilterTest {
+
+}
---
+++ PartVIIII/lesson6/src/test/java/ru/szhernovoy/controllers/UserUpdateTest.java
@@ -0,0 +1,10 @@
+package ru.szhernovoy.controllers;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by admin on 13.12.2016.
+ */
+public class UserUpdateTest {
+
+}
