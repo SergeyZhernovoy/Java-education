1f423ce45a5b701b68e3bd03c11d2e62ec0c50d4
SergeyZhernovoy <s_jernov@mail.ru>
2016-12-23 00:52:40 +0300

    upp lesson 8

--- PartVIIII/lesson8/src/main/webapp/js/myscript.js
+++ PartVIIII/lesson8/src/main/webapp/js/myscript.js
@@ -29,6 +29,57 @@ $(document).ready(function () {
     });
 
 
+    $("#create-btn").click(function () {
+
+        var login = $("#login_field");
+        var password = $("#password_field");
+        var email = $("#email_field");
+        var city = $("#city_field");
+        var country = $("#country_field");
+
+        if (login != '') {
+            $.ajax({
+                url: "create",
+                method: "post",
+                data: {'login': login.val(),
+                       'password': password.val(),
+                       'email':email.val(),
+                       'city': city.val(),
+                       'country': country.val()
+                },
+                datatype: 'json',
+                complete: function (data) {
+                    var result = JSON.parse(data.responseText);
+                    if(Boolean(result.successCreate)){
+                        alert("Success create user")
+                    }
+                    else{
+                        alert("Bad try create user. Try again");
+                    }
+                }
+            });
+        }else{
+            alert("Bad try create user. Try again");
+        }
+    });
+
+    $("#collapse4").click(function () {
+            $.ajax({
+                url: "info",
+                method: "get",
+                datatype: 'json',
+                complete: function (data) {
+                    var result = JSON.parse(data.responseText);
+                    if(Boolean(result.successCreate)){
+                        alert("Success create user")
+                    }
+                    else{
+                        alert("Bad try create user. Try again");
+                    }
+                }
+            });
+    });
+
 });
 
 
