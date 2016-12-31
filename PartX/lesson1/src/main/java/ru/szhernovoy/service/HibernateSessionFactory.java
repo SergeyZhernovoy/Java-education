51ce132c052cc790f42f126d7dc92600e7a0f6ed
SergeyZhernovoy <s_jernov@mail.ru>
2016-12-31 16:15:15 +0300

    update lesson 1 hibernate

---
+++ PartX/lesson1/src/main/java/ru/szhernovoy/service/HibernateSessionFactory.java
@@ -0,0 +1,47 @@
+package ru.szhernovoy.service;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+/**
+ * Created by admin on 30.12.2016.
+ */
+public class HibernateSessionFactory {
+    private final static Logger log = LoggerFactory.getLogger(HibernateSessionFactory.class);
+
+    private static SessionFactory sessionFactory = buildSessionFactory();
+
+    protected static SessionFactory buildSessionFactory() {
+        // A SessionFactory is set up once for an application!
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure() // configures settings from hibernate.cfg.xml
+                .build();
+        try {
+            sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+        }
+        catch (Exception e) {
+            // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+            // so destroy it manually.
+            StandardServiceRegistryBuilder.destroy( registry );
+
+            throw new ExceptionInInitializerError("Initial SessionFactory failed" + e);
+        }
+        return sessionFactory;
+    }
+
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        // Close caches and connection pools
+        getSessionFactory().close();
+    }
+
+
+}
--- PartX/lesson1/src/main/java/ru/szhernovoy/servlets/Validation.java
+++ PartX/lesson1/src/main/java/ru/szhernovoy/servlets/Validation.java
@@ -1,11 +1,39 @@
 package ru.szhernovoy.servlets;
 
+import org.hibernate.Session;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
+import ru.szhernovoy.model.Item;
+import ru.szhernovoy.service.HibernateSessionFactory;
+
+import java.sql.Timestamp;
 
 /**
  * Created by admin on 30.12.2016.
  */
 public class Validation {
     private final static Logger log = LoggerFactory.getLogger(Validation.class);
+
+    public static void main(String[] args) {
+        System.out.println("Hibernate tutorial");
+
+        Session session = HibernateSessionFactory.getSessionFactory().openSession();
+
+        session.beginTransaction();
+
+        Item contactEntity = new Item();
+
+        contactEntity.setCreate(new Timestamp(System.currentTimeMillis()));
+        contactEntity.setDesc("first message");
+        contactEntity.setDone(false);
+
+        session.save(contactEntity);
+        session.getTransaction().commit();
+
+        session.close();
+
+
+    }
+
+
 }
