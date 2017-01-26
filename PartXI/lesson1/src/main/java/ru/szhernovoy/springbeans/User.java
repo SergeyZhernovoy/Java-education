1c55a6dc6753aa7168a5c6bcbe81355e436dd110
SergeyZhernovoy <s_jernov@mail.ru>
2017-01-27 00:39:40 +0300

    add Part XI Spring lesson1

---
+++ PartXI/lesson1/src/main/java/ru/szhernovoy/springbeans/User.java
@@ -0,0 +1,10 @@
+package ru.szhernovoy.springbeans;/**
+ * Created by Admin on 27.01.2017.
+ */
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+public class User {
+    private static final Logger LOGGER = LoggerFactory.getLogger(User.class);
+}
