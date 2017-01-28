29dd0a76e870ea0c73b7b48bfc5ad8c9fdad2a2a
SergeyZhernovoy <s_jernov@mail.ru>
2017-01-28 21:21:11 +0300

    add lesson2 mvc

---
+++ PartXI/lesson2/src/main/java/ru/szhernovoy/carstore/model/Car.java
@@ -0,0 +1,10 @@
+package ru.szhernovoy.carstore.model;/**
+ * Created by Admin on 28.01.2017.
+ */
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+public class Car {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Car.class);
+}
---
+++ PartXI/lesson2/src/main/java/ru/szhernovoy/carstore/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Admin on 28.01.2017.
+ */
+package ru.szhernovoy.carstore;
+
+/**
+ *Реализовать площадку машин на spring mvc
+ */
