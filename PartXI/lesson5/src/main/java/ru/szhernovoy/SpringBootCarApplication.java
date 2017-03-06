369ac49ed8cf3752f9495fd6b9b6234476ba6110
SergeyZhernovoy <s_jernov@mail.ru>
2017-03-06 23:28:36 +0300

    up lesson5 spring boot

--- PartXI/lesson5/src/main/java/ru/szhernovoy/SpringBootCarApplication.java
+++ PartXI/lesson5/src/main/java/ru/szhernovoy/SpringBootCarApplication.java
@@ -4,6 +4,7 @@ package ru.szhernovoy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringBootCarApplication {
 
